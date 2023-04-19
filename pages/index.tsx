f666715b276e8635b76a4fdadd0bd985202b7e3a
Lal Gurung <ljgpok@yahoo.com>
2023-04-19 22:05:54 +0300

    test with pagespeed

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ const Home: NextPage = ({
           />
           <ShowsCollection results={top_ratedShows} title='Top Rated Shows' />
         </div>
-      )}
+      {/* )} */}
     </div>
   );
 };
